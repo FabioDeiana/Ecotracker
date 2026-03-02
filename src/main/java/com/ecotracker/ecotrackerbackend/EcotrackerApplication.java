ff3ae29c241490669c223f2aa9c50d6b5b3d53a6
Fabio Deiana <f.deiana91@gmail.com>
2026-03-02 10:27:44 +0100

    Initial commit

---
+++ src/main/java/com/ecotracker/ecotrackerbackend/EcotrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ecotracker.ecotrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcotrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcotrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecotracker/ecotrackerbackend/EcotrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecotracker.ecotrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcotrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
